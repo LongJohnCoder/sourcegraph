3483f51a3ad12785f1775d5476619f49a69a5095
Eric Fritz <eric@eric-fritz.com>
2019-11-18 17:13:36 -0600

    Create lsif server client struct.

--- enterprise/pkg/codeintel/lsifserver/client/client.go
+++ enterprise/pkg/codeintel/lsifserver/client/client.go
@@ -13,30 +13,30 @@ import (
 	"github.com/opentracing/opentracing-go/ext"
 	"github.com/pkg/errors"
 	"github.com/sourcegraph/sourcegraph/enterprise/pkg/codeintel/lsifserver"
+	"github.com/sourcegraph/sourcegraph/internal/metrics"
 	"github.com/tomnomnom/linkheader"
 )
 
-var httpClient = &http.Client{
-	// nethttp.Transport will propagate opentracing spans
-	Transport: &nethttp.Transport{},
+var requestMeter = metrics.NewRequestMeter("lsif-server", "Total number of requests sent to lsif-server.")
+
+var DefaultClient = &Client{
+	URL: lsifserver.ServerURLFromEnv,
+	HTTPClient: &http.Client{
+		// nethttp.Transport will propagate opentracing spans
+		Transport: &nethttp.Transport{},
+	},
 }
 
-// BuildAndTraceRequest builds a URL and performs a request. This is a convenience wrapper
-// around BuildURL and TraceRequest.
-func BuildAndTraceRequest(ctx context.Context, method, path string, query url.Values, body io.ReadCloser) (*http.Response, error) {
-	url, err := buildURL(path, query)
-	if err != nil {
-		return nil, err
-	}
-
-	return traceRequest(ctx, method, url, body)
+type Client struct {
+	URL        string
+	HTTPClient *http.Client
 }
 
-// TraceRequestAndUnmarshalPayload builds a URL, performs a request, and populates
+// traceRequestAndUnmarshalPayload builds a URL, performs a request, and populates
 // the given payload with the response body. This is a convenience wrapper around
 // BuildURL, TraceRequest, and UnmarshalPayload.
-func TraceRequestAndUnmarshalPayload(ctx context.Context, method, path string, query url.Values, body io.ReadCloser, payload interface{}) error {
-	resp, err := BuildAndTraceRequest(ctx, method, path, query, body)
+func (c *Client) TraceRequestAndUnmarshalPayload(ctx context.Context, method, path string, query url.Values, body io.ReadCloser, payload interface{}) error {
+	resp, err := c.BuildAndTraceRequest(ctx, method, path, query, body)
 	if err != nil {
 		return err
 	}
@@ -44,48 +44,22 @@ func TraceRequestAndUnmarshalPayload(ctx context.Context, method, path string, q
 	return UnmarshalPayload(resp, &payload)
 }
 
-// buildURL constructs a URL to the backend LSIF server with the given path
-// and query values. If path is relative (indicated by a leading slash), then
-// the configured LSIF server url is prepended. Otherwise, it is treated as
-// an absolute URL. The given query values will override any query string that
-// is present in the given path.
-//
-// This method can be used to construct a LSIF request URL either from a root
-// relative path on the first request of a paginated endpoint or from the URL
-// provided by the Link header in a previous response.
-func buildURL(path string, query url.Values) (string, error) {
-	build := url.Parse
-	if len(path) > 0 && path[0] == '/' {
-		build = func(path string) (*url.URL, error) {
-			u, err := url.Parse(lsifserver.ServerURLFromEnv)
-			if err != nil {
-				return nil, err
-			}
-
-			u.Path = path
-			return u, nil
-		}
-	}
-
-	u, err := build(path)
+// buildAndTraceRequest builds a URL and performs a request. This is a convenience wrapper
+// around BuildURL and TraceRequest.
+func (c *Client) BuildAndTraceRequest(ctx context.Context, method, path string, query url.Values, body io.ReadCloser) (*http.Response, error) {
+	url, err := buildURL(c.URL, path, query)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	q := u.Query()
-	for key, values := range query {
-		q.Set(key, values[0])
-	}
-	u.RawQuery = q.Encode()
-
-	return u.String(), nil
+	return c.do(ctx, method, url, body)
 }
 
-// traceRequest performs a GET request to the given URL with the given context. The
+// do performs a GET request to the given URL with the given context. The
 // response is expected to have a 200-level status code. If an error is returned, the
 // HTTP response body has been closed.
-func traceRequest(ctx context.Context, method, url string, body io.ReadCloser) (resp *http.Response, err error) {
-	span, ctx := opentracing.StartSpanFromContext(ctx, "lsifserver.client.traceRequest")
+func (c *Client) do(ctx context.Context, method, url string, body io.ReadCloser) (resp *http.Response, err error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "lsifserver.client.do")
 	defer func() {
 		if err != nil {
 			ext.Error.Set(span, true)
@@ -112,7 +86,7 @@ func traceRequest(ctx context.Context, method, url string, body io.ReadCloser) (
 	// Do not use ctxhttp.Do here as it will re-wrap the request
 	// with a context and this will causes the ot-headers not to
 	// propagate correctly.
-	resp, err = httpClient.Do(req)
+	resp, err = c.HTTPClient.Do(req)
 	if err != nil {
 		if ctx.Err() != nil {
 			err = ctx.Err()
@@ -134,7 +108,44 @@ func traceRequest(ctx context.Context, method, url string, body io.ReadCloser) (
 	return
 }
 
-// UnmarshalPayload reads (and closes) the given response body and populates
+// buildURL constructs a URL to the backend LSIF server with the given path
+// and query values. If path is relative (indicated by a leading slash), then
+// the configured LSIF server url is prepended. Otherwise, it is treated as
+// an absolute URL. The given query values will override any query string that
+// is present in the given path.
+//
+// This method can be used to construct a LSIF request URL either from a root
+// relative path on the first request of a paginated endpoint or from the URL
+// provided by the Link header in a previous response.
+func buildURL(baseURL, path string, query url.Values) (string, error) {
+	build := url.Parse
+	if len(path) > 0 && path[0] == '/' {
+		build = func(path string) (*url.URL, error) {
+			u, err := url.Parse(baseURL)
+			if err != nil {
+				return nil, err
+			}
+
+			u.Path = path
+			return u, nil
+		}
+	}
+
+	u, err := build(path)
+	if err != nil {
+		return "", err
+	}
+
+	q := u.Query()
+	for key, values := range query {
+		q.Set(key, values[0])
+	}
+	u.RawQuery = q.Encode()
+
+	return u.String(), nil
+}
+
+// unmarshalPayload reads (and closes) the given response body and populates
 // the given payload with the JSON response.
 func UnmarshalPayload(resp *http.Response, payload interface{}) error {
 	defer resp.Body.Close()
@@ -147,7 +158,7 @@ func UnmarshalPayload(resp *http.Response, payload interface{}) error {
 	return json.Unmarshal([]byte(body), &payload)
 }
 
-// ExtractNextURL retrieves the URL with rel="next" in the given response's Link
+// extractNextURL retrieves the URL with rel="next" in the given response's Link
 // header. If the link header is empty or has no rel="next", this method returns an
 // empty string.
 func ExtractNextURL(resp *http.Response) string {
